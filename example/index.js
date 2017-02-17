b7900afd110b2d6e8714393ab4d89ff739a83dfc
Ionică Bizău <bizauionica@gmail.com>
2017-02-17 10:29:15 +0200

    :arrow_up: 1.0.6 :tada:

--- example/index.js
+++ example/index.js
@@ -29,31 +29,6 @@ module.exports = {
               , required: true
               , match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, "Please fill a valid email address."]
             }
-          , address: {
-                street: {
-                    type: String
-                  , required: true
-                }
-              , zip: {
-                    type: String
-                  , match: [/\d{5}-?(\d{4})?/, "Invalid ZIP code."]
-                  , required: true
-                }
-              , phone: {
-                    type: String
-                  , match: [/\d{3}[\-]?\d{3}[\-]?\d{4}/, "Invalid phone number format."]
-                  , required: true
-                }
-            }
-          , birth: {
-                type: Date
-              , required: true
-            }
-          , gender: {
-                type: String
-              , match: [/m|f/, "Invalid gender."]
-              , required: true
-            }
         }
     }
 };
